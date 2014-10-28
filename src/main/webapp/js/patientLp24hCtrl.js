e61182a562e5920b6b092d4ac262f754f0a78f45
romanm <roman.mishchenko@gmail.com>
2014-10-28 08:37:11 +0200

    Dialogue drug in the prescriptions document #8 Up, Down, Enter for drug and dose

--- src/main/webapp/js/patientLp24hCtrl.js
+++ src/main/webapp/js/patientLp24hCtrl.js
@@ -211,11 +211,6 @@ insertDrugToTask = function(drug, position, prescribeHistory){
 	$scope.numberOfChange++;
 }
 
-$scope.addDoseToPrescribeDrug = function(doseToPrescribe){
-	$scope.editedPrescribeDrug.dose = doseToPrescribe;
-	$scope.numberOfChange++;
-}
-
 $scope.useHour = function(taskInDay, taskInDayIndex, dayHourIndex, prescribeHistory){
 	if(taskInDay.isCollapsed){
 		var hour =  getLp24hour(dayHourIndex);
@@ -557,8 +552,7 @@ $scope.keys.push({
 	}
 });
 $scope.keys.push({
-	code : KeyCodes.Enter,
-	shiftKey : true,
+	shiftKey : true, code : KeyCodes.Enter,
 	action : function() {
 		$scope.editedPrescribeHistory.selectDrugIndex++;
 		$scope.editedPrescribeHistory.prescribes.tasks.splice($scope.editedPrescribeHistory.selectDrugIndex, 0, null);
@@ -710,7 +704,9 @@ $scope.$on('keydown', function(msg, obj) {
 	var altKey = obj.event.altKey;
 	var shiftKey = obj.event.shiftKey;
 	$scope.keys.forEach(function(o) {
-		if($scope.editedPrescribeHistory.tasksInDay[$scope.editedPrescribeHistory.selectDrugIndex].isCollapsed){
+		if($scope.editedPrescribeHistory.tasksInDay 
+		&& $scope.editedPrescribeHistory.tasksInDay[$scope.editedPrescribeHistory.selectDrugIndex].isCollapsed
+		){
 			return;
 		}
 		if(o.code !== code) return;
@@ -724,120 +720,3 @@ $scope.$on('keydown', function(msg, obj) {
 //---------------------keydown---------------------END-------
 }]);
 
-cuwyApp.controller('taskInDayCtrl', ['$scope', '$http', '$filter', function ($scope, $http, $filter) {
-	console.log("---------taskInDayCtrl------------");
-	$scope.taskInDay.dialogTab = "start";
-	var isJustOpened = true;
-	$scope.selectDrugIndex = null;
-	adaptSelectDrugToFilterList = function(){
-		if(($scope.drug1sListFilter.length-1) < $scope.selectDrugIndex)
-			$scope.selectDrugIndex = null;
-	}
-
-	$scope.filterDrugs = function(){
-		var f1 = $filter('filter')($scope.drug1sList, {DRUG_ARCHIVE:false});
-		var f2 = $filter('filter')(f1, $scope.editedPrescribeDrug.DRUG_NAME);
-		$scope.drug1sListFilter = $filter('limitTo')(f2, 12);
-		adaptSelectDrugToFilterList();
-		if($scope.selectDrugIndex == null)
-			$scope.selectDrugIndex = 0;
-	}
-	$scope.filterDrugs();
-
-	$scope.changeDialogTab = function(fieldName){
-		$scope.taskInDay.dialogTab = fieldName;
-	}
-
-	$scope.drugToTask3 = function(drug){
-		$scope.copyDrugToTask(drug, $scope.editedPrescribeDrug);
-	}
-
-	$scope.copyDrugToTask = function(dS, dT){
-		dT.DRUG_ARCHIVE = dS.DRUG_ARCHIVE;
-		dT.DRUG_ID = dS.DRUG_ID;
-		dT.DRUG_NAME = dS.DRUG_NAME;
-		console.log(1);
-		readDrugDocument(dT);
-		console.log(2);
-		$('#dose1').focus();
-		console.log(3);
-		$scope.numberOfChange++;
-		console.log($scope.taskInDay.dialogTab);
-		$scope.taskInDay.dialogTab = "dose";
-	}
-
-	//-----------------------keydown------------------------
-	var KeyCodes = {
-		Escape : 27,
-		Enter : 13,
-		ArrowUp : 38,
-		ArrowDown : 40
-	};
-
-	$scope.keys = [];
-	$scope.keys.push({
-		code : KeyCodes.Escape,
-		action : function() {
-			console.log("Escape -- taskInDayCtrl");
-			$scope.taskInDay.isCollapsed = false;
-			$scope.patient.pageDeepPositionIndex = 2;
-		}
-	});
-	$scope.keys.push({
-		code : KeyCodes.Enter,
-		shiftKey : true,
-		action : function() {
-			console.log("Enter");
-			if(isJustOpened){
-				isJustOpened = false;
-				return;
-			}
-			$scope.copyDrugToTask($scope.drug1sListFilter[$scope.selectDrugIndex], $scope.editedPrescribeDrug);
-		}
-	});
-	$scope.keys.push({
-		code : KeyCodes.ArrowUp,
-		action : function() {
-			console.log("ArrowUp ");
-			adaptSelectDrugToFilterList();
-			if(null==$scope.selectDrugIndex || 0==$scope.selectDrugIndex){
-				$scope.selectDrugIndex = $scope.drug1sListFilter.length-1;
-			}else{
-				$scope.selectDrugIndex--;
-			}
-		}
-	});
-	$scope.keys.push({
-		code : KeyCodes.ArrowDown,
-		action : function() {
-			console.log("ArrowDown ");
-			adaptSelectDrugToFilterList();
-			if(null==$scope.selectDrugIndex || $scope.selectDrugIndex==($scope.drug1sListFilter.length-1)){
-				$scope.selectDrugIndex = 0;
-			}else{
-				$scope.selectDrugIndex++;
-			}
-		}
-	});
-	$scope.keys.push({
-		code : KeyCodes.ArrowDown,
-		ctrlKey : true,
-		action : function() {
-			console.log("CtrlArrowDown ");
-		}
-	});
-
-	$scope.$on('keydown', function(msg, obj) {
-		//console.log(obj);
-		var code = obj.event.keyCode;
-		var ctrlKey = obj.event.ctrlKey;
-		$scope.keys.forEach(function(o) {
-			if (o.code !== code) return;
-			if(ctrlKey && !o.ctrlKey) return;
-			if(o.ctrlKey && !ctrlKey) return;
-			o.action();
-			$scope.$apply();
-		});
-	});
-
-}]);
