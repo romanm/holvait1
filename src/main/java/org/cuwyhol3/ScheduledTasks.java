41a51a1d48c689fcabb954e0fe4033a56ae286f7
roman.mishchenko@gmail.com <roman.mishchenko@gmail.com>
2014-11-29 12:55:46 +0200

    sheduler save patien drug definition to drug document  v.2

---
+++ src/main/java/org/cuwyhol3/ScheduledTasks.java
@@ -0,0 +1,54 @@
+package org.cuwyhol3;
+
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.List;
+import java.util.Map;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component("scheduledTasks")
+@EnableScheduling
+public class ScheduledTasks {
+	private static final Logger logger = LoggerFactory.getLogger(ScheduledTasks.class);
+	private static final SimpleDateFormat dateFormat = new SimpleDateFormat("HH:mm:ss");
+
+	@Autowired
+	private Lp24jdbc lp24jdbc;
+
+	@Autowired
+	private Lp24ControllerImpl lp24Controller;
+
+	@Scheduled(fixedRate = 27000)
+	public void checkSavedPatient() {
+		final Map<String, Object> readSavedPatient = lp24jdbc.readSavedPatient();
+		logger.debug(""+readSavedPatient+" "+dateFormat.format(new Date()));
+		if(null == readSavedPatient)
+			return;
+		int patientId = (int) readSavedPatient.get("patient_id");
+		Map<String, Object> readJsonDbFile2map = lp24Controller.readJsonDbFile2map(Lp24Config.getPatientDbJsonName(patientId));
+		for (Map map : (List<Map>) readJsonDbFile2map.get("prescribesHistory")) {
+			for (Map drugInPatientDocument : (List<Map>) (List) ((Map) map.get("prescribes")).get("tasks")) {
+				logger.debug(""+drugInPatientDocument);
+				if(null == drugInPatientDocument)
+					continue;
+				final Integer drugId = (Integer) drugInPatientDocument.get("DRUG_ID");
+				final Map<String, Object> readDrugDocument = lp24Controller.readDrug(drugId);
+				if(lp24Controller.updateDrugToBlock1(readDrugDocument, drugInPatientDocument)){
+					lp24Controller.writeToJsonDbFile(readDrugDocument, Lp24Config.getDrugDbJsonName(drugId));
+				}
+			}
+		}
+		lp24jdbc.updateSavedPatientIsChecked(patientId);
+	}
+
+	@Scheduled(fixedRate = 97000)
+	public void reportCurrentTime() {
+		System.out.println("The time is now " + dateFormat.format(new Date()) + lp24jdbc);
+	}
+}
---
+++ src/main/resources/dbVersionUpdateInit.json.sql
@@ -0,0 +1,7 @@
+{
+  "dbVersionUpdateList" : [ {
+    "dbVersionId" : 0,
+    "sqls" : [ "CREATE TABLE if not exists dbversion (dbversion_ID INT(10) NOT NULL, dbversion_date timestamp default now(), primary key (dbversion_id)", "CREATE SEQUENCE  if not exists  dbid" ],
+    "dbVersionDate" : 1416847596280
+  } ]
+}
